e2741327747f02b6e78f69823d2df40d23c827c9
Henry Hilton <hiltonhenrys@gmail.com>
2026-02-14 22:38:52 -0700

    added in main content for each view

--- src/community/community.jsx
+++ src/community/community.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import './community.css'
 
 export function Community() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>community displayed here</div>
-    </main>
+    <main className="container-fluid bg-secondary text-left">
+      <h2>Community</h2>
+
+          <div id="picture" className="picture-box"><img width="400px" height="200px" src="wallstreet.jpg" alt="random" /></div>
+
+          <div className="users">
+          Community Watchlist:
+          <span className="user-name">User1</span>
+          </div>
+          <div id="community-updates">
+              <div className="user-event">Joe started tracking CPI</div>
+              <div className="system-event">Guy logged on</div>
+              <div className="user-event">William started tracking FEDFUNDS</div>
+          </div>
+          <hr />
+
+          <table className="table table-dark">
+              <thead>
+                  <tr>
+                      <th>Name</th>
+                      <th>Last Login</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  <tr>
+                      <td>William Petersen</td>
+                      <td>Jan 19, 2026</td>
+                  </tr>
+                  <tr>
+                      <td>Guy Pierce</td>
+                      <td>Jan 17, 2026</td>
+                  </tr>
+                  <tr>
+                      <td>Joe Pantoliano</td>
+                      <td>Jan 16, 2026</td>
+                  </tr>
+              </tbody>
+          </table>
+  </main>
   );
 }
--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -2,8 +2,31 @@ import React from 'react';
 
 export function Dashboard() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>dashboard displayed here</div>
-    </main>
+    <main className="container-fluid bg-secondary text-left">
+      <h2>Your Dashboard</h2>
+      <div className="row align-items-center">
+          <div className="col-md-6">
+              <h4>Select Your Watchlist</h4>
+                  <form>
+                      <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Consumer Price Index" name="CPI" value="yes"></input>
+                          <label className="form-check-label" for="Consumer Price Index">Display Consumer Price Index data</label>
+                      </div>
+                      <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Federal Funds Rate" name="FEDFUNDS" value="yes"></input>
+                          <label className="form-check-label" for="Federal Funds Rate">Display Federal Funds Rate data</label>
+                      </div>
+                      <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Apple" name="AAPL" value="yes"></input>
+                          <label className="form-check-label" for="Apple">Display Apple data</label>
+                      </div>
+                  </form> 
+          </div>
+          <div className="col-md-6"> 
+              <div id="picture" className="picture-box">
+                  <img width="400px" height="200px" src="CPIfred.png" alt="random" /></div>
+          </div> 
+      </div>
+  </main>
   );
 }
